e4d6a54a20b55f94b36c906389b7b2d70a0ead4f
rudra <rudra@sanctusit.com>
2018-11-13 12:38:51 +0530

    this is final report

--- app.py
+++ app.py
@@ -1,11 +1,66 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app = Flask(__name__)
+app.secret_key = "thIsisaSecret!"
 
+@app.route('/', methods = ['GET','POST'])
+def index():
+    result=request.form.to_dict()
+    #print(result)
+    return render_template('landingpage.html')
+@app.route('/reg', methods = ['GET','POST'])
+def newuser():
 
-@app.route('/')
-def hello():
+    return render_template('index.html')
+@app.route('/create', methods=['POST'])
+def create():
+    #print "In create"
+    error = False
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
+    #print request.form
 
-   return "hello"
+    if len(first_name) < 1:
+        error = True
+        flash('First name cannot be blank')
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+    if first_name.isalpha():
+        flash('First name is usable')
+    else:
+        flash("Please enter a valid First name")
+
+    if last_name.isalpha():
+        flash('Last name is usable')
+    else:
+        flash("Please enter a valid Last name")
+
+    if len(last_name) < 1:
+        error = True
+        flash('Last name cannot be blank')
+
+    if len(email) < 3:
+        error = True
+        flash('Email cannot be blank')
+    if len(password) < 8:
+        error = True
+        flash('Please enter a valid Password')
+    if len(password_confirmation) < 8:
+        error = True
+        flash('Please enter password confirmation')
+    if not EMAIL_REGEX.match(email):
+        error = True
+        flash('Email is invalid')
+    if password != password_confirmation:
+        error = True
+        flash('Passwords do not match')
+
+    if error is True:
+        return redirect('/')
+
+    flash('You Are Now A Registered User!')
+    return redirect('/')
+app.run(debug=True,host='0.0.0.0')
