e0818617d267d064f438d05f7dd15ad1774bd2c4
rudra <rudra@sanctusit.com>
2018-11-16 14:03:14 +0530

    changes app and temp and index also

--- app.py
+++ app.py
@@ -1,65 +1,78 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request,flash,redirect
+import psycopg2
 app = Flask(__name__)
-
+# db = psycopg2.connect(
+#     database = "Dreamland",
+#     user = "postgres",
+#     password = "1234",
+#     host = "localhost"
+# )
+# print(db)
 @app.route('/', methods = ['GET','POST'])
 def index():
-    result=request.form.to_dict()
-    #print(result)
+        try:
+            result = request.form.to_dict()
+            #print(result)
+
+        except:
+            pass
+        try:
+            if result=={}:
+                return render_template('index.html')
+            elif result['password'] != result['c_password']:
+                msg="Passwords do not match"
+                return render_template('reg_user.html',msg=msg)
+            elif result['password'] == result['c_password']:
+                #print(result['first_name'])
+                db = psycopg2.connect(
+                                    database = "dcore2hl3fm13v",
+                                    user = "pnevkxlqdlmdif",
+                                    password = "4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                                    host = "ec2-174-129-192-200.compute-1.amazonaws.com"
+                                )
+                #print(db)
+                cur = db.cursor()
+                cur.execute(
+                    "INSERT INTO test_user1 (First_name,Last_name,Email,Password,Dob,Gender) VALUES ('{}','{}','{}','{}',{},'{}')".format(
+                        result['first_name'], result['last_name'], result['mail'], result['password'],
+                        result['datetimepicker'],
+                        result['gender']))
+                db.commit()
+                #print(" user info created successfully")
+                db.close()
+                msg="You Are Now A Registered User!"
+                return render_template('reg_user.html',msg=msg)
+        except:
+            pass
+@app.route('/reg', methods=['POST'])
+def user_reg():
+    print("In create")
+    error = False
+    result = request.form.to_dict()
+    print(result)
+    if result['password'] != result['c_password']:
+        flash('Passwords do not match')
+        return redirect('/reg')
+    if result['password'] == result['c_password']:
+        print(result['first_name'])
+        db = psycopg2.connect(
+            database="dcore2hl3fm13v",
+            user="pnevkxlqdlmdif",
+            password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+            host="ec2-174-129-192-200.compute-1.amazonaws.com"
+        )
+        cur = db.cursor()
+        cur.execute(
+            "INSERT INTO test_user (First_name,Last_name,Email,Password,Dob,Gender) VALUES ('{}','{}','{}','{}',{},'{}')".format(
+                result['first_name'], result['last_name'], result['mail'], result['password'],
+                result['datetimepicker'],
+                result['gender']))
+        db.commit()
+        print("Records created successfully")
+        db.close()
+    else:
+        flash('You Are Now A Registered User!')
+        return redirect('/')
     return render_template('landingpage.html')
-# @app.route('/reg', methods = ['GET','POST'])
-# def newuser():
-#
-#     return render_template('index.html')
-# @app.route('/create', methods=['POST'])
-# def create():
-#     #print "In create"
-#     error = False
-#     first_name = request.form['first_name']
-#     last_name = request.form['last_name']
-#     email = request.form['email']
-#     password = request.form['password']
-#     password_confirmation = request.form['password_confirmation']
-#     #print request.form
-#
-#     if len(first_name) < 1:
-#         error = True
-#         flash('First name cannot be blank')
-#
-#     if first_name.isalpha():
-#         flash('First name is usable')
-#     else:
-#         flash("Please enter a valid First name")
-#
-#     if last_name.isalpha():
-#         flash('Last name is usable')
-#     else:
-#         flash("Please enter a valid Last name")
-#
-#     if len(last_name) < 1:
-#         error = True
-#         flash('Last name cannot be blank')
-#
-#     if len(email) < 3:
-#         error = True
-#         flash('Email cannot be blank')
-#     if len(password) < 8:
-#         error = True
-#         flash('Please enter a valid Password')
-#     if len(password_confirmation) < 8:
-#         error = True
-#         flash('Please enter password confirmation')
-#     if not EMAIL_REGEX.match(email):
-#         error = True
-#         flash('Email is invalid')
-#     if password != password_confirmation:
-#         error = True
-#         flash('Passwords do not match')
-#
-#     if error is True:
-#         return redirect('/')
-#
-#     flash('You Are Now A Registered User!')
-#     return redirect('/')
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
