5b52f3c58b6321e6bd4df03929e18103b8fb6f0d
rudra <rudra@sanctusit.com>
2018-11-20 13:16:59 +0530

    registration verification mail

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 #     host = "localhost"
 # )
 # print(db)
+db = psycopg2.connect(
+                database="dcore2hl3fm13v",
+                user="pnevkxlqdlmdif",
+                password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                host="ec2-174-129-192-200.compute-1.amazonaws.com"
+            )
+print(db)
 @app.route('/', methods = ['GET','POST'])
 def index():
         try:
@@ -19,29 +26,72 @@ def index():
         try:
             if result=={}:
                 return render_template('index.html')
-            elif result['password'] != result['c_password']:
-                msg="Passwords do not match"
-                return render_template('reg_user.html',msg=msg)
-            elif result['password'] == result['c_password']:
-                #print(result['first_name'])
-                db = psycopg2.connect(
-                                    database = "dcore2hl3fm13v",
-                                    user = "pnevkxlqdlmdif",
-                                    password = "4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
-                                    host = "ec2-174-129-192-200.compute-1.amazonaws.com"
-                                )
-                #print(db)
-                cur = db.cursor()
-                cur.execute(
-                    "INSERT INTO test_user1 (First_name,Last_name,Email,Password,Dob,Gender) VALUES ('{}','{}','{}','{}',{},'{}')".format(
-                        result['first_name'], result['last_name'], result['mail'], result['password'],
-                        result['datetimepicker'],
-                        result['gender']))
-                db.commit()
-                #print(" user info created successfully")
-                db.close()
-                msg="You Are Now A Registered User!"
-                return render_template('reg_user.html',msg=msg)
+        except:
+            pass
+        try:
+            mail_id=result['mail']
+            db = psycopg2.connect(
+                database="dcore2hl3fm13v",
+                user="pnevkxlqdlmdif",
+                password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                host="ec2-174-129-192-200.compute-1.amazonaws.com"
+            )
+            # print(db)
+            # db = psycopg2.connect(
+            #     database="Dreamland",
+            #     user="postgres",
+            #     password="1234",
+            #     host="localhost"
+            # )
+            cur = db.cursor()
+            cur.execute("SELECT email FROM test_user1 where email='{}'".format(mail_id))
+            email_id = cur.fetchone()
+            db.commit()
+            # print(" user info created successfully")
+            db.close()
+            db_mailid=str(email_id)
+            # print(type(db_mailid))
+            # print(type(mail_id))
+            if email_id==None:
+                try:
+                    # print(type(mail_id))
+                    if result['password'] != result['c_password']:
+                        # print(type(mail_id))
+                        msg="Passwords do not match"
+                        return render_template('reg_user.html',msg=msg)
+                    elif result['password'] == result['c_password']:
+                        # print(type(mail_id))
+                        # print(result['first_name'])
+                        db = psycopg2.connect(
+                            database="dcore2hl3fm13v",
+                            user="pnevkxlqdlmdif",
+                            password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                            host="ec2-174-129-192-200.compute-1.amazonaws.com"
+                        )
+                        # db = psycopg2.connect(
+                        #     database="Dreamland",
+                        #     user="postgres",
+                        #     password="1234",
+                        #     host="localhost"
+                        # )
+                        cur = db.cursor()
+                        cur.execute(
+                            "INSERT INTO test_user1 (First_name,Last_name,Email,Password,Dob,Gender) VALUES ('{}','{}','{}','{}',0,0)".format(
+                                result['first_name'], result['last_name'], result['mail'], result['password']))
+                        db.commit()
+                        #print(" user info created successfully")
+                        db.close()
+                        msg="You Are Now A Registered User!"
+                        return render_template('reg_user.html',msg=msg)
+                except:
+                    pass
+            else:
+                # print(db_mailid)
+                # print(mail_id)
+                msg = "The email address you have entered is already registered!"
+                #print(msg)
+                return render_template('reg_user.html', msg=msg)
+
         except:
             pass
 @app.route('/login',methods=['GET', 'POST'])
